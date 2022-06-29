978be67950749b452578d5cf5133d8a673d3d005
meysam <mysm.shahbazi@gmail.com>
2022-06-29 16:32:24 +0430

    add function for parse onnx model and create engine

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include <numeric>
 
 
+using namespace std;
 
 struct  TRTDestroy
 {
@@ -25,6 +26,55 @@ struct  TRTDestroy
     }
 };
 
+class Logger : public nvinfer1::ILogger
+{
+void log(Severity severity, const char* msg) noexcept override
+{
+    // suppress info-level messages
+    if (severity <= Severity::kWARNING)
+        std::cout << msg << std::endl;
+}
+} logger;
+
+
+
+void parseOnnxModel(const string & model_path,
+                    unique_ptr<nvinfer1::ICudaEngine,TRTDestroy> &engine,
+                    unique_ptr<nvinfer1::IExecutionContext,TRTDestroy> &context)
+{
+    // first we create builder 
+    unique_ptr<nvinfer1::IBuilder,TRTDestroy> builder{nvinfer1::createInferBuilder(logger)};
+    // then define flag that is needed for creating network definitiopn 
+    uint32_t flag = 1U <<static_cast<uint32_t>(nvinfer1::NetworkDefinitionCreationFlag::kEXPLICIT_BATCH);
+    unique_ptr<nvinfer1::INetworkDefinition,TRTDestroy> network{builder->createNetworkV2(flag)};
+    // then parse network 
+    unique_ptr<nvonnxparser::IParser,TRTDestroy> parser{nvonnxparser::createParser(*network,logger)};
+    // parse from file
+    parser->parseFromFile(model_path.c_str(),static_cast<int>(nvinfer1::ILogger::Severity::kINFO));
+    for (int32_t i = 0; i < parser->getNbErrors(); ++i)
+    {
+        std::cout << parser->getError(i)->desc() << std::endl;
+    }
+    // lets create config file for engine 
+    unique_ptr<nvinfer1::IBuilderConfig,TRTDestroy> config{builder->createBuilderConfig()};
+
+    config->setMemoryPoolLimit(nvinfer1::MemoryPoolType::kWORKSPACE,1U<<20);
+    config->setMaxWorkspaceSize(1U<<30);
+
+    // use fp16 if it is possible 
+
+    if (builder->platformHasFastFp16())
+    {
+        config->setFlag(nvinfer1::BuilderFlag::kFP16);
+    }
+    // setm max bach size as it is very importannt for trt
+    builder->setMaxBatchSize(1);
+    // create engine and excution context
+    engine.reset(builder->buildEngineWithConfig(*network,*config));
+    context.reset(engine->createExecutionContext());
+
+    return;
+}
 
 int main(int argc, const char ** argv) 
 {
@@ -37,6 +87,11 @@ int main(int argc, const char ** argv)
     std::string model_path = argv[1];
     std::string image_path = argv[2];
     const int batch_size = 1;
+
+    std::unique_ptr<nvinfer1::ICudaEngine,TRTDestroy> engine{nullptr};
+    std::unique_ptr<nvinfer1::IExecutionContext,TRTDestroy> context{nullptr};
+
+    parseOnnxModel(model_path,engine,context);
 
 
 
