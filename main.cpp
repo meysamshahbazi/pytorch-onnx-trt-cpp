896152e3cf79d8d9a8521fdca0f4e03335d6bd9b
meysam <mysm.shahbazi@gmail.com>
2022-06-29 10:17:55 +0430

    start cpp project

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <NvInfer.h>
+#include <memory>
+#include <NvOnnxParser.h>
+#include <vector>
+#include <cuda_runtime_api.h>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core/cuda.hpp>
+#include <opencv2/cudawarping.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/cudaarithm.hpp>
+#include <algorithm>
+#include <numeric>
+
+
+
+int main() 
+{
+
+    return 0;
+}
