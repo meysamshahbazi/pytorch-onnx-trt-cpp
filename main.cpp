a77ec875c3ae87672be6472ea5aaa9e9d5c111bc
meysam <mysm.shahbazi@gmail.com>
2022-06-29 10:30:27 +0430

    custom deleter for trt object

--- main.cpp
+++ main.cpp
@@ -15,8 +15,30 @@
 
 
 
-int main() 
+struct  TRTDestroy
 {
+    template<class T>
+    void operator()(T* obj) const 
+    {
+        if (obj)
+            obj->destroy();
+    }
+};
+
+
+int main(int argc, const char ** argv) 
+{
+    if (argc < 3)
+    {
+        std::cerr<<"usage: " << argv[0] << " model.onnx image.jpg"<<std::endl;
+        return -1;
+    }
+
+    std::string model_path = argv[1];
+    std::string image_path = argv[2];
+    const int batch_size = 1;
+
+
 
     return 0;
 }
